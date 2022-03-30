5c276cb35fc8918c54888f5ffb55cbed7f0e03f2
Jaideep Ghungharwal <jdghungharwal04@gmail.com>
2022-03-30 18:44:32 +0530

    Commiting finished Module 19

---
+++ The Complete 2022 Web Development Bootcamp/intro-to-node/index.js
@@ -0,0 +1,9 @@
+const superheroes = require('superheroes');
+
+const supervillains = require('supervillains');
+
+var mySuperHeroName = superheroes.random();
+
+var mySuperVillainName = supervillains.random();
+
+console.log(mySuperVillainName);
